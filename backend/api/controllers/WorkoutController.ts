f19083c645fc31cd4edfddfdec3d1fe5843da0c9
Gabriel Sousa <gabrielsousa@Gabriels-MacBook-Pro-2.local>
2019-11-21 08:54:18 -0300

    alterqcoes

--- backend/api/controllers/WorkoutController.ts
+++ backend/api/controllers/WorkoutController.ts
@@ -2,6 +2,7 @@ import { Controller, Get, BaseRequest, BaseResponse, HttpError, HttpCode, Post, 
 import Workout from '../models/workout/WorkoutModel';
 import { checkJwt } from '../middlewares/checkJwt';
 import { checkRole } from '../middlewares/checkRole';
+import { patch } from 'semver';
 
 @Controller('/workouts')
 export default class WorkoutController {
@@ -18,6 +19,31 @@ export default class WorkoutController {
       completed: false,
       counter: 0
     });
+
+    return res.success(workout);
+  }
+
+  @Put('/:id', [checkJwt])
+  static async updateWorkout(req, res) {
+
+    const filter = {id: req.params.id};
+    const { name, user, exercises, counter, completed } = req.body;
+    const update = {
+      name: name,
+      user: user,
+      exercises: exercises,
+      counter: counter,
+      completed: completed
+    }
+    const workout = await Workout.findOneAndUpdate(filter, update);
+    return res.success(workout);
+  }
+
+  @Get('/:id', [checkJwt])
+  static async getWorkout(req, res) {
+    
+    const filter = {id: req.params.id};
+    const workout = await Workout.findOne(filter);
     return res.success(workout);
   }
 }
--- backend/api/models/workout/WorkoutModel.ts
+++ backend/api/models/workout/WorkoutModel.ts
@@ -8,22 +8,6 @@ import * as bcrypt from 'bcryptjs';
 class WorkoutModel extends BaseModel {
   static Schema = WorkoutSchema;
 
-  public static async findByEmail(email: string) {
-    return this.findOne({where:{email}})
-  }
-
-  public async setPassword(password: string) {
-    try {
-      this.password = await bcrypt.hash(password, 10)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  public async validatePassword(password: string) {
-    const matchPassword = await bcrypt.compare(password, this.password);
-    return matchPassword
-  }
 }
 
 export default MainDatabase.model(WorkoutModel);
