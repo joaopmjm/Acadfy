5820cae83f582fc37a60f313e810981a8e22f6ce
joaopmjm <joaopmjm@al.insper.edu.br>
2019-12-02 20:46:21 -0300

    DoubtScreen V2.5

--- mobile/Acadfy/components/Doubts/DoubtScreen.js
+++ mobile/Acadfy/components/Doubts/DoubtScreen.js
@@ -2,9 +2,8 @@ import React, {useEffect, useState} from 'react';
 import { Text, TextInput,StyleSheet, View, ScrollView, AsyncStorage, KeyboardAvoidingView } from 'react-native';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { ListItem, Button } from 'react-native-elements';
-import api from "../../services/api";
 import Icon from 'react-native-vector-icons/Ionicons';
-import { nominalTypeHack } from 'prop-types';
+import Axios from 'axios';
 
 function DoubtScreen(){
   var [msgs, setMsgs] = useState([]);
@@ -12,30 +11,32 @@ function DoubtScreen(){
   const [name, setName] = useState("");
   const [user_id, setUserId] = useState("");
   const [role, setRole] = useState("");
-  const [coach_id, setCoachId] = useState("");
-
+  const [admin_id, setAdminId] = useState("");
+  
+ 
   const getCoachId = async () => {
-      await api.post("/consumer/trainer",{
-        locals:{
-          userId:{
-            id:user_id,
-          }
+    let token = await AsyncStorage.getItem('token')
+        try {
+            const response = await Axios({
+                url: 'http://107.20.116.185/consumer/trainer',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': token,
+                }
+            })
+            console.log("Coach ID"+response.data);
+            setAdminId(response.data)
+        } catch (error) {
+            console.error(error);
         }
-    }).then((response)=> response.json())
-    .then((responseJSON)=>{
-      setCoachId(responseJSON.id)
-    }).catch((error) => {
-      console.error(error);
-    })
-  }
-
+  };
   const getUserData = async () => {
+      console.log("Getting User Data");
       const get_name = await AsyncStorage.getItem("name");
       const get_usr_id = await AsyncStorage.getItem("id");
       const get_role = await AsyncStorage.getItem("role");
-      const get_token = await AsyncStorage.getItem("token");
       setName(get_name);
-      setToken(get_token);
       getCoachId();
       setUserId(get_usr_id);
       setRole(get_role);
@@ -43,27 +44,36 @@ function DoubtScreen(){
     
   const getHistory = async () => {
     // get Hist of chat
-    const response = await api.post('mensage/history',{
-      body:{
-      admin_id: coach_id,
-      user_id : user_id,
-      }
-    }).then((response) => response.json())
-    .then((responseJSON) => {
-      setMsgs(responseJSON);
-    }).catch((error) => {
-      console.error(error);
-    });
+    let token = await AsyncStorage.getItem('token')
+        try {
+            const response = await Axios({
+                url: 'http://107.20.116.185/message/history',
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': token,
+                },
+                body:{
+                  'user_id':user_id,
+                  'admin_id':admin_id,
+                }
+            })
+            const resJSON = JSON.stringify(response.data);
+            console.log("Historico "+ resJSON);
+            //setMsgs(resJSON.data);
+        } catch (error) {
+            console.error(error);
+        }
   };
 
   const handleSend = async () => {
-    // send msg, no momento soh atualiza localmente como o backend ainda nao esta finalizado
+    // // send msg, no momento soh atualiza localmente como o backend ainda nao esta finalizado
     if (role === "admin"){
       //para o professor mandar
-      var new_msg = [{msg : text.text,author: name},{msg : "Hard coded response", author:"Server"}];
+      var new_msg = [{msg : text.text,author: name}];
     }else{
       //para o usuario mandar
-      var new_msg = [{msg : text.text,author: name},{msg : "Hard coded response", author:"Server"}];
+      var new_msg = [{msg : text.text,author: name}];
     }
     var new_hist = msgs.concat(new_msg);
     setText("");
@@ -71,43 +81,62 @@ function DoubtScreen(){
 
     // actual coding after this line
     if (role === "admin"){
-      await api.post('mensage/admin_mensage',{
-        body:{
-          name: name,
-          role:"admin",
-          msn:text,
-          admin_id:coach_id,
-          user_id:user_id,
+      let token = await AsyncStorage.getItem('token')
+        try {
+            const response = await Axios({
+                url: 'http://107.20.116.185/message/admin-mensage',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': token,
+                },
+                body:{
+                  'name': name,
+                  'role':"admin",
+                  'msn':text.text,
+                  'admin_id':admin_id,
+                  'user_id':user_id,
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        } finally{
+          console.log("Added msg: " + text.text);
+          setText("");
         }
-      }).then((response)=>{
-        console.log(response);
-      }).catch((error)=>{
-        console.error(error);
-      })
 
     }else{
-      await api.post('mensage/user_mensage',{
-        body:{
-          name: name,
-          role:"user",
-          msn:text,
-          admin_id:coach_id,
-          user_id:user_id,
+      let token = await AsyncStorage.getItem('token')
+      const msn = text.text
+        try {
+            const response = await Axios({
+                url: 'http://107.20.116.185/message/user-message',
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': token,
+                },
+                body:{
+                  'name': name,
+                  'role':"consumer",
+                  'msn':msn,
+                  'admin_id':admin_id,
+                  'user_id':user_id, 
+                }
+            })
+            console.log("Exited sending")
+        } catch (error) {
+            console.error(error);
+        }finally{
+          console.log("Added msg: " + text.text);
+          setText("");
         }
-      }).then((response)=>{
-        console.log(response);
-      }).catch((error)=>{
-        console.error(error);
-      })
-
-    }
-
-  };
+  };}
 
   useEffect(()=>{
     if (name === ""){getUserData();}
     getHistory();
-  },[msgs,text]);
+  });
     
   return (
     <KeyboardAvoidingView style={styles.page}>
